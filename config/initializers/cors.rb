792377642a95234e27ed281253556fddbf11bf58
Your Name <my@email.org>
2024-11-25 09:38:27 +0000

    deployment to EC2

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://54.234.92.34"
+    origins "http://44.207.216.157"
     resource "*", headers: :any, methods: [ :get, :post, :put, :patch, :delete, :options, :head ],
       credentials: true
   end
