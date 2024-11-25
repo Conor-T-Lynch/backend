1e2c5e6da0f8554db02332bc08fbd660cbd830c6
Your Name <my@email.org>
2024-11-25 14:10:26 +0000

    Seconf commit

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://3.223.144.88'
-    resource '*', headers: :any, methods: [ :get, :post, :put, :patch, :delete, :options, :head ],
+    origins "http://3.223.144.88"
+    resource "*", headers: :any, methods: [ :get, :post, :put, :patch, :delete, :options, :head ],
       credentials: true
   end
 end
